13deff9a38b15608387a12b4362cba66f047db48
Test-Github-explore <158149339+Test-Github-explore@users.noreply.github.com>
2024-02-08 17:42:21 +0900

    Add files via upload

--- FIO30.c
+++ FIO30.c
@@ -1,22 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
 
-void incorrect_password(const char *user) {
-  int ret;
-  /* ユーザ名は256文字以下に制限されている */
-  static const char msg_format[] = "%s cannot be authenticated.\n";
-  size_t len = strlen(user) + sizeof(msg_format);
-  char *msg = (char *)malloc(len);
-  if (msg == NULL) {
-    /* エラー処理 */
-  }
-  ret = snprintf(msg, len, msg_format, user);
-  if (ret < 0) {
-    /* エラー処理 */
-  } else if (ret >= len) {
-    /* 切り捨てられた出力の処理 */
-  }
-  fprintf(stderr, msg);
-  free(msg);
+int main(void){
+   printf("Hello World\n");
+
+   return 0;
 }
